94531141acfd4d38e6c5089d62b5cedf69c2ec23
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-07-13 17:38:26 -0300

    finish create transaction

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -14,7 +14,7 @@ type TransactionInput  = Pick<Transaction, 'title' | 'amount' | 'type' | 'catego
 
 interface TransactionContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 interface TransactionProviderProps {
@@ -33,8 +33,17 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   }, []);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transactions', transaction)
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date()
+    })
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction  
+    ]);
   }
 
   return (
